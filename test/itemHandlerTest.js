c44663948b68b8a6c6635fd0fc7498364330fa9d
Estefania Chavez Guardado <fachinacg@gmail.com>
2018-06-12 12:05:11 +0200

    Create new list test

---
+++ test/itemHandlerTest.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+const ItemHandler = require('../ItemHandler');
+
+describe('Item Handler', () => {
+  const itemHandler = new ItemHandler();
+  describe('#createNewList', () => {
+    it('should return a new List', () => {
+      assert.notEqual(itemHandler.getList().length, 0);
+    });
+  }); 
+});
