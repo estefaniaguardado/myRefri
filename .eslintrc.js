41987b661d671826b2462d693395f83fca57cded
Estefania Chavez Guardado <fachinacg@gmail.com>
2018-06-27 19:36:25 +0200

    Fixing CRLF with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
-module.exports = {
+const os = require('os');
+
+const config = {
     "extends": "airbnb-base",
-    "rules": {
-        "linebreak-style": [
-          "error",
-          "windows"
-        ]
-      },
+    "rules": {},
     "env": {
       "mocha": true,
     }
 };
+
+// Fix CRLF || LF issues
+if (os.platform().startsWith('win')) {
+  config.rules["linebreak-style"] = [
+    "error",
+    "windows"
+  ];
+}
+
+module.exports = config;
