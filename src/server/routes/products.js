da872b64a77613d85a5dd786ace586db0c15cd1d
Estefania Chavez Guardado <fachinacg@gmail.com>
2018-07-26 16:52:54 +0200

    Jsdocs productsRouter

--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -2,10 +2,23 @@ const router = require('express-promise-router')();
 
 const productHandler = require('../services/ProductHandler')();
 
+/**
+ * @memberof Router.products
+ * @name get/products/id
+ * @function
+ * Router serving product details by id product through the request.
+ * @inner
+ * @param {String} path - Express path
+ * @param {callback} middleware - Express middleware
+ * @return {json} Id product and available unities
+ */
 router.get('/:id', (req, res) => {
   const product = productHandler.findProductById(req.params.id);
 
   return res.json({ id: req.params.id, unities: product.unities });
 });
 
+/**
+ * @namespace Router.products
+ */
 module.exports = router;
