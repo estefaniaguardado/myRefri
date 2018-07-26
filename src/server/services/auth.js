0e1c1941fda349f3eda1790b0f3affe38e9317b5
Estefania Chavez Guardado <fachinacg@gmail.com>
2018-07-26 16:11:26 +0200

    Add jsdocs auth service

--- src/server/services/auth.js
+++ src/server/services/auth.js
@@ -4,6 +4,15 @@ const passport = require('passport');
 const userHandler = require('./userHandler');
 
 // Note: Move if more strategies are added
+/**
+ * Load passport-level middleware to authenticate by LocalStrategy for username/password.
+ * @function
+ * @callback LocalStrategy - Authenticate if the given user already exists through LocalStrategy.
+ * @param {String} username
+ * @param {String} password
+ * @param {} done - Verify callback and return user if found it.
+ * @see {@link http://www.passportjs.org/} Passportjs
+ */
 passport.use(new LocalStrategy(async (username, password, done) => {
   try {
     const user = await userHandler.findUserByName(username);
@@ -15,9 +24,24 @@ passport.use(new LocalStrategy(async (username, password, done) => {
   }
 }));
 
-
+/**
+ * Passport will serialize user instance to and from the session.
+ * @function
+ * @callback SerializeUser - The user ID is serialized to the session.
+ * @param {JSON} user
+ * @param {} done - Verify callback and return user.id, which will be restored in req.user.
+ * @see {@link http://www.passportjs.org/docs/configure/} Passportjs
+ */
 passport.serializeUser((user, done) => done(null, user.id));
 
+/**
+ * Passport will deserialize user instance to and from the session.
+ * @function
+ * @callback DeserializeUserID - Deserialize the user ID to find the user.
+ * @param {String} id
+ * @param {} done - Verify callback and return user by user.id from req.user.
+ * @see {@link http://www.passportjs.org/docs/configure/} Passportjs
+ */
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await userHandler.findUserById(id);
