4040f5427d44249efd32269261ac60b16190ca42
Estefania Chavez Guardado <fachinacg@gmail.com>
2018-09-27 13:14:29 +0200

    Reset schema database

--- src/server/db/connector.ts
+++ src/server/db/connector.ts
@@ -1,18 +1,21 @@
-import { Pool } from 'pg';
+import pgPromise from 'pg-promise';
 
-const pool = new Pool({
+const initOptions = {
+  connect(client) {
+    const cp = client.connectionParameters;
+    console.log('Connected to database:', cp.database);
+  },
+};
+
+const config = {
   user: process.env.USER || 'postgres',
   host: 'localhost',
-  database: process.env.USER || 'postgres',
+  database: process.env.USER || 'myRefri',
   password: '12345',
   port: 5432,
-});
+};
 
-pool.connect();
+const pgp = pgPromise(initOptions);
+const db = pgp(config);
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  pool.end();
-});
-
-export default pool;
+export = db;
---
+++ src/server/db/schema/index.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import fs from 'fs';
+import * as db from '../connector';
+
+async function restoreDatabase() {
+  const createSql = fs.readFileSync(path.join(__dirname, './create.sql'), 'utf-8');
+  const dropSql = fs.readFileSync(path.join(__dirname, './drop.sql'), 'utf-8');
+
+  try {
+    await db.none(dropSql);
+    await db.none(createSql);
+
+    console.log('Create schema');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+restoreDatabase();
