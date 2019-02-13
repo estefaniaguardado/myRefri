56f6f07b58fe3a9a35cf1f388c825bf809d1e719
EstefaniaGuardado <fachinacg@gmail.com>
2019-02-13 15:38:52 +0100

    Fix constructor properties DetailsError

--- src/server/DetailsError.ts
+++ src/server/DetailsError.ts
@@ -2,22 +2,7 @@ import ExtendableError from 'es6-error';
 
 export default class DetailsError extends ExtendableError {
 
-  /**
-   * HTTP response status code by the server to the client
-   */
-  statusCode: number;
-
-  /**
-   * Description to show in the client
-   */
-  description: string;
-
-  /**
-   * Details about the info that can help to fix
-   */
-  details: string;
-
-  constructor(readonly name: string, message: string, code: number, description: string, details: string) {
+  constructor(readonly name: string, readonly message: string, readonly statusCode: number, readonly description: string, readonly details: string) {
     super(message);
   }
 }
